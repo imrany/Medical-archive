efd004f78dc1b1e1f9e12365f75b86158d082143
imrany <imranmat254@gmail.com>
2023-05-20 13:44:13 +0300

    updates

--- src/pages/Topic.tsx
+++ src/pages/Topic.tsx
@@ -19,11 +19,11 @@ function Topic() {
     return (
         <div className='flex flex-col justify-center items-center px-[5vw] my-[5vh] lg:my-[10vh]' key={slug}>
            <div className='flex text-sm my-4' title="Patience's question">
-                <span className='text-xl font-bold mr-4'><i className='ri-user-line ri-2x'></i>:</span>  
+                <span className='text-lg font-bold mr-4'>Q:</span>  
                 <p><span className='font-bold text-xl'>{details.question.slice(0,1).toUpperCase()}</span>{details.question.slice(1)}</p>
            </div>
            <div className='flex text-sm my-4' title="Doctor's response">
-                <span className='text-xl font-bold mr-4'><i className='ri-hospital-line ri-2x'></i>:</span>  
+                <span className='text-lg font-bold mr-4'>A:</span>  
                 <p><span className='font-bold text-xl'>{details.answer.slice(0,1).toUpperCase()}</span>{details.answer.slice(1)}</p>
            </div>
         </div>
